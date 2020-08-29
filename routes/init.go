b17669fb5f3e291afb466774831a9a1c33fbd80b
Jonathan Thom <jonathanthom@hey.com>
2020-08-29 14:57:22 -0700

    Add logging middleware for routes

--- routes/init.go
+++ routes/init.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	middleware "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	"github.com/jonathanwthom/earl/handlers"
@@ -13,12 +14,22 @@ func Init(db *gorm.DB) {
 	r := mux.NewRouter()
 	h := handlers.Init(db)
 
-	// @todo: Add generic logging middleware around routes
 	// Order matters
-	r.HandleFunc("/links", h.GetLinksHandler).Methods("GET")
-	r.HandleFunc("/{identifier}", h.GetLinkHandler).Methods("GET")
-	r.HandleFunc("/links", h.CreateLinkHandler).Methods("POST")
-	r.HandleFunc("/accounts", h.CreateAccountHandler).Methods("POST")
+	r.Handle(
+		"/links", middleware.LoggingHandler(os.Stdout, http.HandlerFunc(h.GetLinksHandler)),
+	).Methods("GET")
+	r.Handle(
+		"/{identifier}",
+		middleware.LoggingHandler(os.Stdout, http.HandlerFunc(h.GetLinkHandler)),
+	).Methods("GET")
+	r.Handle(
+		"/links",
+		middleware.LoggingHandler(os.Stdout, http.HandlerFunc(h.CreateLinkHandler)),
+	).Methods("POST")
+	r.Handle(
+		"/accounts",
+		middleware.LoggingHandler(os.Stdout, http.HandlerFunc(h.CreateAccountHandler)),
+	).Methods("POST")
 
 	port := os.Getenv("PORT")
 	if port == "" {
