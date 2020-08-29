9b2fd0f6767649ff9d653a239929f40ab87d5388
Jonathan Thom <jonathanthom@hey.com>
2020-08-29 14:48:33 -0700

    Make shortened required in db

--- models/link.go
+++ models/link.go
@@ -12,7 +12,7 @@ type Link struct {
 	gorm.Model `json:"-"`
 	Original   string `gorm:"not null" json:"original""`
 	Identifier string `gorm:"unique;not null" json:"-"`
-	Shortened  string `gorm:"unique" json:"shortened"`
+	Shortened  string `gorm:"unique; not null" json:"shortened"`
 	AccountID  uint   `json:"-"`
 	Views      []View `json:"views"`
 }
