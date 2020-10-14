bd8ca92a14c1b77d38ae939059fa0339f387d08e
Jonathan Thom <jonathanthom@hey.com>
2020-10-13 20:45:39 -0700

    Extract getAccountFromToken in createLinkHandler

--- handlers/create_link_handler.go
+++ handlers/create_link_handler.go
@@ -34,23 +34,33 @@ func createLinkHandler(w http.ResponseWriter, req *http.Request) {
 	w.Write(js)
 }
 
+func getAccountFromToken(req *http.Request) (*models.Account, error) {
+	account := &models.Account{}
+	auth := req.Header.Get("Authorization")
+
+	if auth != "" {
+		token := strings.ReplaceAll(auth, "basic ", "")
+		notFound := db.Where("token = ?", token).First(account).RecordNotFound()
+		if notFound {
+			return account, errors.New("No account with token")
+		}
+
+		return account, nil
+	}
+
+	return account, errors.New("Missing Authorization header")
+}
+
 func createLink(original string, req *http.Request) (*models.Link, error) {
 	link := &models.Link{Original: original}
 
-	// share header code
-	auth := req.Header.Get("Authorization")
-	if auth != "" {
-		token := strings.ReplaceAll(auth, "basic ", "")
-		account := &models.Account{}
-		notFound := db.Where("token = ?", token).First(account).RecordNotFound()
-		if notFound {
-			return link, errors.New("No account with token")
-		}
-
-		link.AccountID = account.ID
+	account, err := getAccountFromToken(req)
+	if err != nil {
+		return link, err
 	}
+	link.AccountID = account.ID
 
-	err := link.Validate()
+	err = link.Validate()
 	if err != nil {
 		log.Print(err)
 		return link, errors.New("Invalid parameter: url")
